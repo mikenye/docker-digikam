5051633094704eb1b98fd6c284ff6d482aee31a1
Mike Nye <mike.nye@xpanse.com.au>
2020-04-23 09:08:04 +0800

    Include push

--- build.sh
+++ build.sh
@@ -10,11 +10,15 @@ IMAGE=digikam
 # Build on x86_64
 docker context use x86_64
 
-# Build & push latest
-docker build -t ${REPO}/${IMAGE}:latest --compress --pull .
+# Build latest
+docker build -t "${REPO}/${IMAGE}:latest" --compress --pull .
 
 # Get version from latest
 VERSION=$(docker run --rm --entrypoint cat ${REPO}/${IMAGE}:latest /VERSIONS | grep digikam | cut -d " " -f 2)
 
-# Build & push version-specific
+# Build version-specific
 docker build -t "${REPO}/${IMAGE}:${VERSION}" --compress --pull .
+
+# Push
+docker push "${REPO}/${IMAGE}:${VERSION}"
+docker push "${REPO}/${IMAGE}:latest"
